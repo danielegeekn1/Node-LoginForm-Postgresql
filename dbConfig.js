fc0d439bce5f754995d22893cff491deac2f0333
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-18 11:17:27 +0100

    connected postresgl database to our project

---
+++ dbConfig.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+const isProduction = process.env.NODE_ENV === "production";
+//connect our postgresql database to our project
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
