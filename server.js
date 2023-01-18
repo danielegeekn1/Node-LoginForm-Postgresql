05bff942ec94c3c04915f5646b69e8a7473b3597
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-18 10:55:52 +0100

    added main routes

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.send("Hello ");
+  res.render("index");
+});
+app.get("/users/register", (req, res) => {
+  res.render("register");
+});
+app.get("/users/login", (req, res) => {
+  res.render("login");
+});
+app.get("/users/dashboard", (req, res) => {
+  res.render("dashboard", { user: "Conor" });
 });
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
