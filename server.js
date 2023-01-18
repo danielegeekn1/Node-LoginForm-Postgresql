7b3244d38e67d6439fe616a2fb6bd4e788da6da9
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-18 12:06:10 +0100

    added some checks in our register form

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const { pool } = require("./dbConfig");
 const port = process.env.PORT || 4000;
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false })); //allows us to send data from frontend to our server
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -13,6 +15,26 @@ app.get("/users/login", (req, res) => {
 });
 app.get("/users/dashboard", (req, res) => {
   res.render("dashboard", { user: "Conor" });
+});
+app.post("/users/register", (req, res) => {
+  let { email, name, password, password2 } = req.body;
+  console.log({ email, name, password, password2 });
+  let errors = [];
+  //check wether all field of our form are filled
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: "please enter all fields" });
+  }
+  //check wether the password is at least of 6 characters
+  if (password.lenght < 6) {
+    errors.push({ message: "password should be at least 6 characters" });
+  }
+  //check wether the passwords match correctly
+  if (password !== password2) {
+    errors.push({ message: "password must match" });
+  }
+  if (errors.lenght > 0) {
+    res.render("register", { errors });
+  }
 });
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
